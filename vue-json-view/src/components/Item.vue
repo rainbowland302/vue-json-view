718a82a444643f042bd9d0c9e215e3ca53284a5f
CORP\hel11 <lisa.he@emc.com>
2017-10-13 16:02:02 +0800

    add double click event listen to open all folders recursively

--- vue-json-view/src/components/Item.vue
+++ vue-json-view/src/components/Item.vue
@@ -3,16 +3,29 @@
     <div :class="{nest: isNest}" @click="toggle">
       <span class="item-key">{{`${displayKey}: `}}</span>
       <span class="item-value">{{displayValue}}</span>
-      <span class="item-toggle" v-if="isNest">[{{open ? '-' : '+'}}]</span>
+      <span class="item-toggle" v-if="isNest">[{{isOpen ? '-' : '+'}}]</span>
     </div>
-    <ul v-show="open" v-if="isNest">
-      <item
-        class="item"
-        v-for="(value, key) in model"
-        @clearExpandAll="clearExpandAll"
-        :model="value"
-        :keyName="key"
-        :isExpandAll="isExpandAll"
+    <ul v-if="expandAllElement">
+      <item class="item" 
+        v-for="(value, key) in model" 
+        @toggleExpandAll="toggleExpandAll"
+        @toggleOpen="toggleOpen"
+        :model="value" 
+        :keyName="key" 
+        :expandAll=true 
+        :open=true
+        :key="key">
+      </item>
+    </ul>
+   <ul v-else-if="notExpandAllElement">
+      <item class="item" 
+        v-for="(value, key) in model" 
+        @toggleExpandAll="toggleExpandAll"
+        @toggleOpen="toggleOpen"
+        :model="value" 
+        :keyName="key" 
+        :expandAll=false
+        :open=false
         :key="key">
       </item>
     </ul>
@@ -25,21 +38,30 @@ export default {
   props: {
     model: Object | String,
     keyName: String | Number,  // "key" is a reserved attribute and cannot be used as component prop
-    isExpandAll: Boolean
+    expandAll: Boolean,
+    open:Boolean
   },
-  data: function () {
+  data: function() {
     return {
-      expand: false
+      innerExpand: false,
+      innerOpen:false,
+      clicks: 0,
+      delay: 700,
+      timer: null
     }
   },
+  created: function(){
+    this.innerOpen = this.open;
+    this.innerExpand = this.expandAll;
+  },
   computed: {
-    isObject: function () {
+    isObject: function() {
       return typeof this.model === 'object' && !Array.isArray(this.model)
     },
-    isArray: function () {
+    isArray: function() {
       return Array.isArray(this.model)
     },
-    isNest: function () {
+    isNest: function() {
       return typeof this.model === 'object'
     },
     displayKey: function() {
@@ -48,21 +70,53 @@ export default {
     displayValue: function() {
       return this.isObject ? 'Object' : (this.isArray ? 'Array' : this.model)
     },
-    open: function() {
-      if(this.isExpandAll) this.expand = true;
-      return this.expand;
+    isOpen: function() {
+      return this.innerOpen;
+    },
+    expandAllElement: function(){
+      return  this.isNest && this.innerOpen && this.innerExpand
+    },
+    notExpandAllElement: function(){
+      return this.isNest && this.innerOpen && !this.innerExpand
     }
+
   },
   methods: {
-    toggle: function () {
-      if (this.isNest) {
-        this.expand = !this.expand;
-        this.clearExpandAll();
+    toggle: function() {
+      this.clicks++;
+      if (this.clicks === 1) {
+        this.timer = setTimeout(() => {
+          if (this.isNest) {
+            this.innerOpen = !this.innerOpen;
+            this.toggleOpen();
+          }
+          if(this.innerExpand){
+            this.toggleExpandAll();
+            this.innerExpand = !this.innerExpand;
+          }
+          this.clicks=0;
+        }, this.delay)
+
+      }
+      else{
+        clearTimeout(this.timer);
+        if(this.innerOpen === this.innerExpand){
+          this.innerExpand = !this.innerExpand;
+          this.toggleExpandAll();
+        }
+        if (this.isNest) {
+            this.innerOpen = !this.innerOpen;
+            this.toggleOpen();
+        }
+          this.clicks=0;
       }
     },
-    clearExpandAll: function() {
-      this.$emit('clearExpandAll');
-    }
+    toggleOpen: function(){
+      this.$emit('toggleOpen');
+    },
+    toggleExpandAll: function(){
+      this.$emit('toggleExpandAll');
+    },
   }
 }
 </script>
@@ -76,14 +130,17 @@ body {
 .nest {
   cursor: pointer;
 }
+
 ul {
   padding-left: 1em;
   line-height: 1.5em;
   /* list-style-type: dot; */
 }
+
 .item-key {
   color: deepskyblue;
 }
+
 .item-toggle {
   font-weight: bold;
 }
